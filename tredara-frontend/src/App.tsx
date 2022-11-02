3c1356c39f13c74d7bf418e579a5cfc50ee38a77
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2022-11-02 11:35:17 +0100

    "TRED-33" Added components and pages

--- tredara-frontend/src/App.tsx
+++ tredara-frontend/src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Footer from "./components/Footer";
+import NavigationBar from "./components/NavigationBar";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import "./styles/style.css";
 
 export default function App() {
-  return <h1>Welcome to Tredara</h1>;
+  return (
+    <div className="App">
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/register" element={<SignUp />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
