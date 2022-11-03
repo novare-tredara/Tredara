0cd503dc742b0a84384848bd2627163fc12666d6
mallikaravi <mailtomallikaravi@gmail.com>
2022-11-03 15:43:02 +0100

    Adding Navigation bar

--- tredara-frontend/src/components/NavigationBar.tsx
+++ tredara-frontend/src/components/NavigationBar.tsx
@@ -1,10 +1,32 @@
-import Search from "./Search";
+import { Link } from "react-router-dom";
+import Logo from "assets/images/logo-icon.png";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 export default function NavigationBar() {
   return (
     <nav className="navigation-bar">
-      <h1>Nav Bar</h1>
-      
+      <img src={Logo} />
+      <div className="d-flex justify-content-start">
+        <div className="col-md-5">
+          <input
+            id="search"
+            className="form-control full-width form-rounded"
+            type="search"
+            placeholder="Search..."
+            aria-label="Search"
+          />
+        </div>
+      </div>
+
+      <Link to="/NewListing">NewListing</Link>
+      <Link to="/Accessories">Accessories</Link>
+      <Link to="/BeautyCare">BeautyCare</Link>
+      <Link to="/Books">Books</Link>
+      <Link to="/Mobiles">Mobiles</Link>
+      <Link to="/Clothes">Clothes</Link>
+      <Link to="/Login">Login</Link>
+      <Link to="/Signup">Signup</Link>
     </nav>
   );
 }
